3b7d0cc4a59d13fda026467c1188ccf1cc62a007
dangkl123 <nhatdang2604@gmail.com>
2022-09-02 14:36:25 +0700

    Add xml approach to register bean

--- src/main/java/com/example/springcourse/SpringCourseApplication.java
+++ src/main/java/com/example/springcourse/SpringCourseApplication.java
@@ -2,16 +2,18 @@ package com.example.springcourse;
 
 import com.example.config.AppConfig;
 import com.example.service.IStudentService;
+import com.example.service.StudentService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 //@SpringBootApplication
 public class SpringCourseApplication {
 
     public static void main(String[] args) {
-//        1st approach
+        //1st approach: Java code
 //        ApplicationContext ctx
 //                = new AnnotationConfigApplicationContext(AppConfig.class);
 //
@@ -19,14 +21,20 @@ public class SpringCourseApplication {
 //        System.out.println(studentService.getAll());
 
         //2nd approach
-        AnnotationConfigApplicationContext ctx
-                = new AnnotationConfigApplicationContext();
+//        AnnotationConfigApplicationContext ctx
+//                = new AnnotationConfigApplicationContext();
+//
+//        ctx.register(AppConfig.class);
+//        ctx.refresh();
+//
+//        IStudentService studentService = ctx.getBean(IStudentService.class);
+//        System.out.println(studentService.getAll());
 
-        ctx.register(AppConfig.class);
-        ctx.refresh();
+        //3rd approach: xml config
+        ApplicationContext ctx =
+                new ClassPathXmlApplicationContext("services.xml");
 
-        IStudentService studentService = ctx.getBean(IStudentService.class);
+        IStudentService studentService = ctx.getBean(StudentService.class);
         System.out.println(studentService.getAll());
     }
-
 }
