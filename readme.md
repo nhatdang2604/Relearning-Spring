35cd4916ceeed8db131f1b6a2198f5964b834178
dangkl123 <nhatdang2604@gmail.com>
2022-09-10 16:24:02 +0700

    Add beans for CDPlayerConfig class for config explicity

--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
-# Resource on learning Spring framework in 2022
+# Resource on learning Spring framework
 - [Spring Core](https://docs.spring.io/spring-framework/docs/current/reference/html/core.html#spring-core)
 - Spring in Action 4th Edition
--- spring-in-action-4th-edition/chap_2/src/main/java/com/example/config/CDPlayerConfig.java
+++ spring-in-action-4th-edition/chap_2/src/main/java/com/example/config/CDPlayerConfig.java
@@ -1,12 +1,32 @@
 package com.example.config;
 
+import com.example.soundsystem.CDPlayer;
 import com.example.soundsystem.CompactDisc;
+import com.example.soundsystem.MediaPlayer;
+import com.example.soundsystem.SgtPeppers;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 //@ComponentScan(basePackages = {"com.example.soundsystem"})
-@ComponentScan(basePackages = "com.example.soundsystem")
+//@ComponentScan(basePackages = "com.example.soundsystem")  //for implicit wiring
 public class CDPlayerConfig {
 
+    @Bean(name="lonelyHeartsClubBand")
+    public CompactDisc sgtPeppers() {
+        return new SgtPeppers();
+    }
+
+    //1st approach
+//    @Bean
+//    public MediaPlayer cdPlayer() {
+//        return new CDPlayer(sgtPeppers());
+//    }
+
+    //2nd approach
+    @Bean
+    public MediaPlayer cdPlayer(CompactDisc disc) {
+        return new CDPlayer(disc);
+    }
 }
