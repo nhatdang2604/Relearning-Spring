6788a9063ce063814696bb1a715c0a4355e3298a
dangkl123 <nhatdang2604@gmail.com>
2022-09-10 14:13:10 +0700

    Add SgtPepper as CompactDisc

---
+++ spring-in-action-4th-edition/chap_2/src/main/java/com/example/soundsystem/SgtPeppers.java
@@ -0,0 +1,17 @@
+package com.example.soundsystem;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SgtPeppers implements CompactDisc {
+
+    private String title = "Sgt. Pepper's Lonely Hearts Club Band";
+    private String artist = "The Beatles";
+
+    @Override
+    public void play() {
+        System.out.println("Playing " + title + " by " + artist);
+    }
+}
