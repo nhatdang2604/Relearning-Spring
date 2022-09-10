b55e45f374a7e30e1ed15234d1ce2294aa8b081f
dangkl123 <nhatdang2604@gmail.com>
2022-09-10 15:34:49 +0700

    Add CDPlayer as MediaPlayer for using the @Autowired on all args constructor

---
+++ spring-in-action-4th-edition/chap_2/src/main/java/com/example/soundsystem/CDPlayer.java
@@ -0,0 +1,23 @@
+package com.example.soundsystem;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+@NoArgsConstructor
+public class CDPlayer implements MediaPlayer {
+
+    private CompactDisc cd;
+
+    @Autowired
+    public CDPlayer(CompactDisc cd) {
+        this.cd = cd;
+    }
+
+    @Override
+    public void play() {
+
+    }
+}
