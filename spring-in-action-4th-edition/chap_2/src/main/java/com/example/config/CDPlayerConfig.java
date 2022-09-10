351e73f9a876ca36e31d719c83d793706ea45583
dangkl123 <nhatdang2604@gmail.com>
2022-09-10 15:13:39 +0700

    Change the criteria of the @ComponentScan in CDPlayerConfig, from 'package' to 'class'

--- spring-in-action-4th-edition/chap_2/src/main/java/com/example/config/CDPlayerConfig.java
+++ spring-in-action-4th-edition/chap_2/src/main/java/com/example/config/CDPlayerConfig.java
@@ -1,10 +1,12 @@
 package com.example.config;
 
+import com.example.soundsystem.CompactDisc;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = {"com.example.soundsystem"})
+//@ComponentScan(basePackages = {"com.example.soundsystem"})
+@ComponentScan(basePackageClasses = CompactDisc.class)
 public class CDPlayerConfig {
 
 }
