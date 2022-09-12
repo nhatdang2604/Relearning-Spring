50e1e067647e9e4accd03b2d8c133144dd3cd757
dangkl123 <nhatdang2604@gmail.com>
2022-09-12 16:32:17 +0700

    Mixing the configuration style with java annotation and xml, using @ImportResource with @Import in the SoundSystemConfig.class

--- spring-in-action-4th-edition/chap_2/src/main/java/com/example/config/SoundSystemConfig.java
+++ spring-in-action-4th-edition/chap_2/src/main/java/com/example/config/SoundSystemConfig.java
@@ -5,9 +5,11 @@ import com.example.soundsystem.SgtPeppers;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportResource;
 
 @Configuration
-@Import({CDConfig.class, CDPlayerConfig.class})
+@Import({CDPlayerConfig.class})
+@ImportResource("classpath:CDConfig.xml")
 public class SoundSystemConfig {
 
 }
