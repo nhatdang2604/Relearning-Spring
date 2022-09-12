1041b4cac47e10cb78a2a665dc2f9ed463b62d67
dangkl123 <nhatdang2604@gmail.com>
2022-09-12 15:31:45 +0700

    Add a collection property for Blank Disc

--- spring-in-action-4th-edition/chap_2/src/main/java/com/example/soundsystem/BlankDisc.java
+++ spring-in-action-4th-edition/chap_2/src/main/java/com/example/soundsystem/BlankDisc.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 @AllArgsConstructor
@@ -11,9 +13,13 @@ public class BlankDisc implements CompactDisc {
 
     private String title;
     private String artist;
+    private List<String> tracks;
 
     @Override
     public void play() {
         System.out.println("Playing " + title +  " by " + artist);
+        tracks.forEach(track -> {
+           System.out.println("- Track: " + track);
+        });
     }
 }
