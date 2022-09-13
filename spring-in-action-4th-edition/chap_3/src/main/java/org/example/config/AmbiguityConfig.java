4bdc4b8954d28ca96224763e4678c1baed6aca23
dangkl123 <nhatdang2604@gmail.com>
2022-09-13 23:00:37 +0700

    Add package scanning for AmbiguityConfig

--- spring-in-action-4th-edition/chap_3/src/main/java/org/example/config/AmbiguityConfig.java
+++ spring-in-action-4th-edition/chap_3/src/main/java/org/example/config/AmbiguityConfig.java
@@ -2,9 +2,11 @@ package org.example.config;
 
 import org.example.ambiguity.Dish;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan(basePackages="org.example.ambiguity")
 public class AmbiguityConfig {
 
     @Bean
