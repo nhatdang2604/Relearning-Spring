1a13e40ec2678f4212e52e10fb39ea119078913a
dangkl123 <nhatdang2604@gmail.com>
2022-09-16 01:01:52 +0700

    Add config.xml for enabling auto-proxing with the xml style

--- spring-in-action-4th-edition/chap_4/src/main/java/org/example/config/AOPConfig.java
+++ spring-in-action-4th-edition/chap_4/src/main/java/org/example/config/AOPConfig.java
@@ -2,11 +2,13 @@ package org.example.config;
 
 import org.example.concert.Audience;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
 @EnableAspectJAutoProxy
+@ComponentScan(basePackages="org.example")
 public class AOPConfig {
 
     @Bean
