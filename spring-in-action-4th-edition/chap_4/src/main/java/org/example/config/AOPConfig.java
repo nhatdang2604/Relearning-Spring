cf17eb59e4b822fef9b9e73bba98610e9e8ae39f
dangkl123 <nhatdang2604@gmail.com>
2022-09-16 00:54:06 +0700

    enable the auto-proxying by add @EnableAspectJAutoProxy on the AOPConfig class

--- spring-in-action-4th-edition/chap_4/src/main/java/org/example/config/AOPConfig.java
+++ spring-in-action-4th-edition/chap_4/src/main/java/org/example/config/AOPConfig.java
@@ -3,8 +3,10 @@ package org.example.config;
 import org.example.concert.Audience;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
+@EnableAspectJAutoProxy
 public class AOPConfig {
 
     @Bean
