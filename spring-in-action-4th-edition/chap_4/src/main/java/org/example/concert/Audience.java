4a46d6bc96760f5dca38375943c96ed612f77e25
dangkl123 <nhatdang2604@gmail.com>
2022-09-16 00:49:33 +0700

    Add usage of @pointcut to reference for the pointcut on Audience class

--- spring-in-action-4th-edition/chap_4/src/main/java/org/example/concert/Audience.java
+++ spring-in-action-4th-edition/chap_4/src/main/java/org/example/concert/Audience.java
@@ -1,29 +1,29 @@
 package org.example.concert;
 
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.AfterThrowing;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 
 @Aspect
 public class Audience {
 
-    @Before("execution(* org.example.concert.Performance.perform(..))")
+    @Pointcut("execution(* org.example.concert.Performance.perform(..))")
+    public void performance() {}
+
+    @Before("performance()")
     public void silenceCellPhones() {
         System.out.println("Silencing cell phones");
     }
 
-    @Before("execution(* org.example.concert.Performance.perform(..))")
+    @Before("performance()")
     public void takeSeats() {
         System.out.println("Taking seats");
     }
 
-    @AfterReturning("execution(* org.example.concert.Performance.perform(..))")
+    @AfterReturning("performance()")
     public void applause() {
         System.out.println("CLAP CLAP CLAP!!!");
     }
 
-    @AfterThrowing("execution(* org.example.concert.Performance.perform(..))")
+    @AfterThrowing("performance()")
     public void demandRefund() {
         System.out.println("Demanding a refund!");
     }
