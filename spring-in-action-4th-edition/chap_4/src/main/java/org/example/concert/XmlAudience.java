021a6edc7fe234acedd8fd90359e6ff320c19fba
dangkl123 <nhatdang2604@gmail.com>
2022-09-19 13:50:32 +0700

    Add XmlAudience to show the usage of aspect in xml style

---
+++ spring-in-action-4th-edition/chap_4/src/main/java/org/example/concert/XmlAudience.java
@@ -0,0 +1,23 @@
+package org.example.concert;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class XmlAudience {
+
+    public void silenceCellPhones() {
+        System.out.println("Silencing cell phones");
+    }
+
+    public void takeSeats() {
+        System.out.println("Taking seats");
+    }
+
+    public void applause() {
+        System.out.println("CLAP CLAP CLAP!!!");
+    }
+
+    public void demandRefund() {
+        System.out.println("Demanding a refund!");
+    }
+}
