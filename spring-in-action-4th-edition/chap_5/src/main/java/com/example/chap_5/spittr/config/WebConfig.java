2150b719cd42902a82759f958b4efe8922a91040
dangkl123 <nhatdang2604@gmail.com>
2022-09-20 15:52:18 +0700

    Add viewResolver() as bean for jsp in WebConfig class

--- spring-in-action-4th-edition/chap_5/src/main/java/com/example/chap_5/spittr/config/WebConfig.java
+++ spring-in-action-4th-edition/chap_5/src/main/java/com/example/chap_5/spittr/config/WebConfig.java
@@ -1,9 +1,39 @@
 package com.example.chap_5.spittr.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
+@PropertySource(value="classpath:application.properties")
+@ComponentScan(basePackages="com.example.chap_5.spittr.web")
 public class WebConfig {
+
+    @Autowired
+    private Environment env;
+
+    @Bean
+    public ViewResolver viewResolver() {
+
+        InternalResourceViewResolver resolver =
+                new InternalResourceViewResolver();
+
+        String prefix = env.getProperty("view.prefix");
+        String suffix = env.getProperty("view.suffix");
+
+        resolver.setPrefix(prefix);
+        resolver.setSuffix(suffix);
+
+        //making bind the bean as ${} in jsp
+        resolver.setExposeContextBeansAsAttributes(true);
+
+        return resolver;
+    }
 }
