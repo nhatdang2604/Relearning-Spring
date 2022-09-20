de74a2d5d218016519206ca14ba2ddb7480a3947
dangkl123 <nhatdang2604@gmail.com>
2022-09-20 15:10:03 +0700

    Reinit the chap_5, as a spring web project

---
+++ spring-in-action-4th-edition/chap_5/src/test/java/com/example/chap_5/Chap5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chap_5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chap5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
