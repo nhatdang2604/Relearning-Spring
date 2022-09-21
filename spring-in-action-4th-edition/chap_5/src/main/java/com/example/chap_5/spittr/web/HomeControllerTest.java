933ccddd6c148130115f11f7378286671c8e8bb2
dangkl123 <nhatdang2604@gmail.com>
2022-09-21 17:06:24 +0700

    Add mockito test for HomeController by using HomeControllerTest

--- spring-in-action-4th-edition/chap_5/src/main/java/com/example/chap_5/spittr/web/HomeControllerTest.java
+++ spring-in-action-4th-edition/chap_5/src/main/java/com/example/chap_5/spittr/web/HomeControllerTest.java
@@ -1,15 +1,38 @@
 package com.example.chap_5.spittr.web;
 
+import com.example.chap_5.spittr.config.RootConfig;
+import com.example.chap_5.spittr.config.WebConfig;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.TestPropertySources;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 public class HomeControllerTest {
 
     @Test
     public void testHomePage() throws Exception {
         HomeController homeController = new HomeController();
-        assertEquals("home", homeController.home());
+
+        MockMvc mockMvc =
+                standaloneSetup(homeController)
+                        .addPlaceholderValue("path.home", "/")
+                        .build();
+
+        mockMvc.perform(get("/"))
+                .andExpect(view().name("home"));
     }
 }
