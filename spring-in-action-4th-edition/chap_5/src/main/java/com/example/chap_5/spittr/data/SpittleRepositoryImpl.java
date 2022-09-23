fc24d56a5e40d480379fa82a3f70ef159105ffb7
dangkl123 <nhatdang2604@gmail.com>
2022-09-23 13:04:24 +0700

    Add SpittleController

---
+++ spring-in-action-4th-edition/chap_5/src/main/java/com/example/chap_5/spittr/data/SpittleRepositoryImpl.java
@@ -0,0 +1,13 @@
+package com.example.chap_5.spittr.data;
+
+import com.example.chap_5.spittr.Spittle;
+
+import java.util.List;
+
+public class SpittleRepositoryImpl implements SpittleRepository {
+    @Override
+    public List<Spittle> findSpittles(long max, int count) {
+
+        return null;
+    }
+}
