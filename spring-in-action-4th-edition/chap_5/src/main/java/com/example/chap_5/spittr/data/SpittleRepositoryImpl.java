fc46b7aedc0b01e6514a3dbf7348f3eb27d4af7f
dangkl123 <nhatdang2604@gmail.com>
2022-09-23 13:12:29 +0700

    Add SpittleRepositoryImpl

--- spring-in-action-4th-edition/chap_5/src/main/java/com/example/chap_5/spittr/data/SpittleRepositoryImpl.java
+++ spring-in-action-4th-edition/chap_5/src/main/java/com/example/chap_5/spittr/data/SpittleRepositoryImpl.java
@@ -1,13 +1,41 @@
 package com.example.chap_5.spittr.data;
 
 import com.example.chap_5.spittr.Spittle;
+import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
+@Repository
 public class SpittleRepositoryImpl implements SpittleRepository {
+
+    List<Spittle> spittles;
+
+    public SpittleRepositoryImpl() {
+
+        spittles = new LinkedList<>();
+        mockData();
+    }
+
+    private void mockData() {
+        int size = 100;
+        for (long i = 0; i < size; ++i) {
+            spittles.add(new Spittle(i, "test " + i, new Date(i), new Double(i), new Double(i)));
+        }
+    }
+
     @Override
     public List<Spittle> findSpittles(long max, int count) {
 
-        return null;
+        List<Spittle> buffer = spittles
+                .stream()
+                .filter(spittle -> spittle.getId()<max)
+                .limit(count)
+                .collect(Collectors.toList());
+
+        return buffer;
     }
 }
