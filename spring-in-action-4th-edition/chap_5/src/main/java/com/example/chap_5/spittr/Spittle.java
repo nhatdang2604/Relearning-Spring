6bd5758a2df5f2d4c69cfa1e1ebb5a643ecf4f0b
dangkl123 <nhatdang2604@gmail.com>
2022-09-23 12:53:09 +0700

    Add Spittle model

---
+++ spring-in-action-4th-edition/chap_5/src/main/java/com/example/chap_5/spittr/Spittle.java
@@ -0,0 +1,32 @@
+package com.example.chap_5.spittr;
+
+import lombok.*;
+
+import java.util.Date;
+import java.util.Objects;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Spittle {
+
+    private Long id;
+    private String message;
+    private Date time;
+    private Double latitude;
+    private Double longitude;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Spittle spittle = (Spittle) o;
+        return id.equals(spittle.id) && time.equals(spittle.time);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, time);
+    }
+}
