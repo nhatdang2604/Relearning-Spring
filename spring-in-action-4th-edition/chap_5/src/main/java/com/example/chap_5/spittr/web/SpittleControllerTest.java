23fd1f9db5eed402a5d8dd991c771a7dd819d523
dangkl123 <nhatdang2604@gmail.com>
2022-09-23 17:41:51 +0700

    Add Test for /spittle/show?spittle-id=xxx

--- spring-in-action-4th-edition/chap_5/src/main/java/com/example/chap_5/spittr/web/SpittleControllerTest.java
+++ spring-in-action-4th-edition/chap_5/src/main/java/com/example/chap_5/spittr/web/SpittleControllerTest.java
@@ -3,20 +3,42 @@ package com.example.chap_5.spittr.web;
 import com.example.chap_5.spittr.Spittle;
 import com.example.chap_5.spittr.data.SpittleRepository;
 import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.test.web.servlet.setup.StandaloneMockMvcBuilder;
 import org.springframework.web.servlet.view.InternalResourceView;
 
 import java.util.*;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class SpittleControllerTest {
+
+
+    private Map<String, String> placeholders;
+
+    //Construct the placeholder for each test
+    @BeforeEach
+    public void buildPlaceholder() {
+        placeholders = new HashMap<>();
+        placeholders.put("path.spittles", "/spittles");
+        placeholders.put("path.spittles.show", "/show");
+    }
+
+    //Add the placeholder with <key, value>, to the MockMvcBuilder
+    private StandaloneMockMvcBuilder placeholderBuilder(Object controller) {
+        StandaloneMockMvcBuilder builder = MockMvcBuilders.standaloneSetup(controller);
+        for (Map.Entry<String, String> entry: placeholders.entrySet()) {
+            builder = builder.addPlaceholderValue(entry.getKey(), entry.getValue());
+        }
+
+        return builder;
+    }
 
     private List<Spittle> createSpittleList(long count) {
         List<Spittle> spittles = new ArrayList<>();
@@ -27,6 +49,7 @@ class SpittleControllerTest {
 
         return spittles;
      }
+
 
     @Test
     public void shouldShowRecentSpittles() throws Exception {
@@ -42,13 +65,8 @@ class SpittleControllerTest {
 
         SpittleController controller = new SpittleController(mockRepository);
 
-        //Placeholders support
-        Map<String, String> placeholders = new HashMap<>();
-        placeholders.put("path.spittles", "/spittles");
-
-        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller)
+        MockMvc mockMvc = placeholderBuilder(controller)
                 .setSingleView(new InternalResourceView("/WEB-INF/views/spittles.jsp"))
-                .addPlaceholderValue("path.spittles", placeholders.get("path.spittles"))
                 .build();
 
         String spittleListKey = "spittleList";
@@ -59,5 +77,25 @@ class SpittleControllerTest {
                 .andExpect(MockMvcResultMatchers.model().attribute(spittleListKey,
                         Matchers.hasItems(expectedSpittles.toArray())));
 
+    }
+
+    @Test
+    public void testSpittle() throws Exception {
+
+        long spittleId = 0;
+        Spittle expectedSpittle =
+                new Spittle(spittleId, "test " + spittleId, new Date(spittleId), new Double(spittleId), new Double(spittleId));
+
+        SpittleRepository mockRepository = Mockito.mock(SpittleRepository.class);
+        Mockito.when(mockRepository.findOne(0))
+                .thenReturn(expectedSpittle);
+
+        SpittleController spittleController = new SpittleController(mockRepository);
+        MockMvc mockMvc = placeholderBuilder(spittleController).build();
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/spittles/show?spittle-id=" + spittleId))
+                .andExpect(MockMvcResultMatchers.view().name("spittle"))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("spittle"))
+                .andExpect(MockMvcResultMatchers.model().attribute("spittle", expectedSpittle));
     }
 }
