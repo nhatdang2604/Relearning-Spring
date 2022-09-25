93df21a9f7f6e39f8eb0bb2a2eed6e982fca6ba7
dangkl123 <nhatdang2604@gmail.com>
2022-09-25 15:11:51 +0700

    Add Post request handler on submiting Spitter Form

--- spring-in-action-4th-edition/chap_5/src/main/java/com/example/chap_5/spittr/web/SpitterController.java
+++ spring-in-action-4th-edition/chap_5/src/main/java/com/example/chap_5/spittr/web/SpitterController.java
@@ -1,16 +1,34 @@
 package com.example.chap_5.spittr.web;
 
+import com.example.chap_5.spittr.Spitter;
+import com.example.chap_5.spittr.data.SpitterRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("${path.spitter}")
+@AllArgsConstructor
 public class SpitterController {
 
-    @RequestMapping(value="${path.spitter.register}", method= RequestMethod.GET)
+    @Value("${path.spitter}")
+    private final String ROOT;
+
+    private SpitterRepository spitterRepository;
+
+    @RequestMapping(value="${path.spitter.register}", method=RequestMethod.GET)
     public String showRegistrationForm() {
         return "registerForm";
+    }
+
+    @RequestMapping(value="${path.spitter.register}", method=RequestMethod.POST)
+    public String processRegistration(Spitter spitter) {
+
+        spitterRepository.save(spitter);
+
+        return "redirect:" + ROOT + "/" + spitter.getUsername();
     }
 
 }
