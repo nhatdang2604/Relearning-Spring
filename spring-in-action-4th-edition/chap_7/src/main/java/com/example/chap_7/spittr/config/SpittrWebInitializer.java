f9f6f6fc134bf29bd1a35ae1c8768640818036ab
dangkl123 <nhatdang2604@gmail.com>
2022-10-05 21:41:27 +0700

    Add configuration for multipart on SpittrWebInitializer

--- spring-in-action-4th-edition/chap_7/src/main/java/com/example/chap_7/spittr/config/SpittrWebInitializer.java
+++ spring-in-action-4th-edition/chap_7/src/main/java/com/example/chap_7/spittr/config/SpittrWebInitializer.java
@@ -22,11 +22,11 @@ public class SpittrWebInitializer
         return new String[] {"/"};
     }
 
-//    @Override
-//    protected void customizeRegistration(ServletRegistration.Dynamic registration) {
-//        registration.setMultipartConfig(
-//                new MultipartConfigElement("/tmp/spittr/uploads")
-//        );
-//    }
+    @Override
+    protected void customizeRegistration(ServletRegistration.Dynamic registration) {
+        registration.setMultipartConfig(
+                new MultipartConfigElement("/tmp/spittr/uploads")
+        );
+    }
 }
 
