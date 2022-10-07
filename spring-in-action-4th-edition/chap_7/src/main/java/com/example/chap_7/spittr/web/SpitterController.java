a73b14d2e58c1380ed40041315573b6f7e479e64
dangkl123 <nhatdang2604@gmail.com>
2022-10-07 14:31:46 +0700

    Add usage of RedirectAttribute for using .addFlashAttribute, to bind the spitter object to the model, making it survived after redirecting

--- spring-in-action-4th-edition/chap_7/src/main/java/com/example/chap_7/spittr/web/SpitterController.java
+++ spring-in-action-4th-edition/chap_7/src/main/java/com/example/chap_7/spittr/web/SpitterController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.Part;
 import javax.validation.Valid;
@@ -23,6 +24,7 @@ public class SpitterController {
 
     private static final String PARAM_SPITTER_USERNAME = "username";
     private static final String PARAM_SPITTER_ID = "spitterId";
+    private static final String PARAM_SPITTER = "spitter";
 
     @Value("${path.spitter}")
     private String ROOT;
@@ -44,7 +46,7 @@ public class SpitterController {
             @RequestPart("profilePicture") Part profilePicture,
             @Valid Spitter spitter,
             Errors errors,
-            Model model) {
+            RedirectAttributes model) {
 
         if (errors.hasErrors()) {
             return "registerForm";
@@ -53,7 +55,8 @@ public class SpitterController {
         spitterRepository.save(spitter);
         spitterRepository.saveFile(profilePicture);
         model.addAttribute(PARAM_SPITTER_USERNAME, spitter.getUsername());
-        model.addAttribute(PARAM_SPITTER_ID, spitter.getId());
+//        model.addAttribute(PARAM_SPITTER_ID, spitter.getId());
+        model.addFlashAttribute(PARAM_SPITTER, spitter);
         return "redirect:" + ROOT + "/" + "{" + PARAM_SPITTER_USERNAME + "}";
     }
 
